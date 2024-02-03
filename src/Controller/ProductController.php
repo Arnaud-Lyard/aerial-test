9a7283d1765f1cd2a72ce2f3e0c2b01710c23cdd
Arnaud-Lyard <arnaud.lyard@gmail.com>
2024-02-03 11:36:40 +0100

    ✨ create modal to add products and product controller

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+
+    /**
+     * @Route("/create-product", name="create_product")
+     */
+    public function createProduct(Request $request, ManagerRegistry $doctrine): Response
+    {
+
+        $name = $request->request->get('name');
+        $description = $request->request->get('description');
+        $priceExcludingTax = $request->request->get('priceExcludingTax');
+        $valueAddedTax = $request->request->get('valueAddedTax');
+        $image = $request->request->get('image');
+
+        if (empty($name) || empty($description) || empty($priceExcludingTax) || empty($valueAddedTax) || empty($image)) {
+            return new JsonResponse(['success' => false, 'message' => 'All fields are required']);
+        }
+
+        $entityManager = $doctrine->getManager();
+        $product = new Product();
+        $product->setName($name);
+        $product->setDescription($description);
+        $product->setPriceExcludingTax($priceExcludingTax);
+        $product->setValueAddedTax($valueAddedTax);
+        $product->setImage($image);
+
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        return new JsonResponse(['success' => true]);
+    }
+}
