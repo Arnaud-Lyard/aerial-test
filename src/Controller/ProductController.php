f04240e7efc8665db92d91d445422876609c3f69
Arnaud-Lyard <arnaud.lyard@gmail.com>
2024-02-03 16:31:01 +0100

    ✨ refresh new product after submit modal

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -13,33 +13,39 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductController extends AbstractController
 {
 
-    /**
-     * @Route("/create-product", name="create_product")
-     */
-    public function createProduct(Request $request, ManagerRegistry $doctrine): Response
-    {
+  /**
+   * @Route("/create-product", name="create_product")
+   */
+  public function createProduct(Request $request, ManagerRegistry $doctrine): Response
+  {
 
-        $name = $request->request->get('name');
-        $description = $request->request->get('description');
-        $priceExcludingTax = $request->request->get('priceExcludingTax');
-        $valueAddedTax = $request->request->get('valueAddedTax');
-        $image = $request->request->get('image');
+    $name = $request->request->get('name');
+    $description = $request->request->get('description');
+    $priceExcludingTax = $request->request->get('priceExcludingTax');
+    $valueAddedTax = $request->request->get('valueAddedTax');
+    $image = $request->request->get('image');
 
-        if (empty($name) || empty($description) || empty($priceExcludingTax) || empty($valueAddedTax) || empty($image)) {
-            return new JsonResponse(['success' => false, 'message' => 'All fields are required']);
-        }
-
-        $entityManager = $doctrine->getManager();
-        $product = new Product();
-        $product->setName($name);
-        $product->setDescription($description);
-        $product->setPriceExcludingTax($priceExcludingTax);
-        $product->setValueAddedTax($valueAddedTax);
-        $product->setImage($image);
-
-        $entityManager->persist($product);
-        $entityManager->flush();
-
-        return new JsonResponse(['success' => true]);
+    if (empty($name) || empty($description) || empty($priceExcludingTax) || empty($valueAddedTax) || empty($image)) {
+      return new JsonResponse(['success' => false, 'message' => 'All fields are required']);
     }
+
+    $entityManager = $doctrine->getManager();
+    $product = new Product();
+    $product->setName($name);
+    $product->setDescription($description);
+    $product->setPriceExcludingTax($priceExcludingTax);
+    $product->setValueAddedTax($valueAddedTax);
+    $product->setImage($image);
+
+    $entityManager->persist($product);
+    $entityManager->flush();
+
+    return new JsonResponse(['success' => true, 'product' => [
+      'name' => $product->getName(),
+      'description' => $product->getDescription(),
+      'priceExcludingTax' => $product->getPriceExcludingTax(),
+      'valueAddedTax' => $product->getValueAddedTax(),
+      'image' => $product->getImage(),
+    ]]);
+  }
 }
